2715a8a73064e43c4b636994452662c6bb9b5dd6
Anton P <polevyy.anton@gmail.com>
2022-07-22 18:32:16 +1000

    Created initial tests

---
+++ src/deposit/__tests__/operations.spec.js
@@ -0,0 +1,33 @@
+import { validateInput, calculateFinalBalance } from '../operations';
+
+describe('operationsTest', () => {
+  test('input validation', () => {
+    const correctInput =  {
+      startDeposit: 1000000,
+      interestRate: 1.1,
+      investmentTerm: 36,
+      interestPaid: 'Monthly'
+    };
+    expect(validateInput(correctInput)).toBe(true);
+
+    const incorrectInput = {
+      ...correctInput,
+      interestPaid: 'Daily'
+    };
+    expect(() => validateInput(incorrectInput)).toThrow();
+
+    const { startDeposit, ...missingValueInput } = correctInput;
+    expect(() => validateInput(startDeposit)).toThrow();
+    expect(() => validateInput(missingValueInput)).toThrow();
+  });
+
+  test('balance calculation', () => {
+    const input =  {
+      startDeposit: 1000000,
+      interestRate: 1.1,
+      investmentTerm: 36,
+      interestPaid: 'At Maturity'
+    };
+    expect(calculateFinalBalance(input)).toBe(1033000);
+  });
+});
