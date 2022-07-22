884a374f783b9e8a6f35efccf16248bfdfbf9b8d
Anton P <polevyy.anton@gmail.com>
2022-07-22 18:24:14 +1000

    Setup initial project structure

--- src/deposit/operations.js
+++ src/deposit/operations.js
@@ -1,7 +1,23 @@
-export const calculate = () => {
-  return 100;
+const getInputFromJson = (jsonFile) => {
+  const rawData = require(jsonFile);
+  return {
+    startDeposit: rawData['start-deposit'],
+    interestRate: rawData['interest-rate'],
+    investmentTerm: rawData['investment-term'],
+    interestPaid: rawData['interest-paid']
+  };
 };
 
-// module.exports = {
-//   calculate
-// }
+const validateInput = (input) => {
+  return false;
+};
+
+const calculateFinalBalance = (input) => {
+  return 0;
+};
+
+module.exports = {
+  getInputFromJson,
+  validateInput,
+  calculateFinalBalance
+};
