8ea9924f2cb0bf029ab47ce7095f0aafb0e8de13
Anton P <polevyy.anton@gmail.com>
2022-07-22 19:26:59 +1000

    Added tests for balance calculation

--- src/deposit/__tests__/operations.spec.js
+++ src/deposit/__tests__/operations.spec.js
@@ -28,12 +28,44 @@ describe('operationsTest', () => {
   });
 
   test('balance calculation', () => {
-    const input =  {
+    const input = {
       startDeposit: 1000000,
       interestRate: 1.1,
       investmentTerm: 36,
+      interestPaid: 'Monthly'
+    };
+    expect(calculateFinalBalance(input)).toBe(1033500);
+
+    const input2 = {
+      startDeposit: 1000000,
+      interestRate: 2.2,
+      investmentTerm: 36,
+      interestPaid: 'Quarterly'
+    };
+    expect(calculateFinalBalance(input2)).toBe(1068000);
+
+    const input3 = {
+      startDeposit: 1000000,
+      interestRate: 3,
+      investmentTerm: 30,
       interestPaid: 'At Maturity'
     };
-    expect(calculateFinalBalance(input)).toBe(1033000);
+    expect(calculateFinalBalance(input3)).toBe(1075000);
+
+    const input4 = {
+      startDeposit: 1000000,
+      interestRate: 3,
+      investmentTerm: 37,
+      interestPaid: 'Annually'
+    };
+    expect(calculateFinalBalance(input4)).toBe(1095400);
+
+    const input5 = {
+      startDeposit: 1000000,
+      interestRate: 4.5,
+      investmentTerm: 37,
+      interestPaid: 'Quarterly'
+    };
+    expect(calculateFinalBalance(input5)).toBe(1147900);
   });
 });
